0f98b992a68069f33f6c388689047fcf28f43442
xtrnen <jtrnenyx@gmail.com>
2017-11-18 15:23:52 +0100

    Revert "Doplnění src podle Masteru"
    This reverts commit fb22dcac886b51e89500eaa29ea00ed0371ecc13.

--- src/token.h
+++ src/token.h
@@ -11,10 +11,7 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-extern char* TokenTypes[];
-
 typedef enum {
-  TK_NA = 0, // Token niceho
   TK_COMMA, // ,
   TK_EOF, // konec souboru
   TK_EOL, // znak konce radku
@@ -64,7 +61,6 @@ typedef enum {
   // ciselne konstanty
   TK_NUM_INTEGER,
   TK_NUM_DOUBLE,
-  TK_NUM_STRING,
 
   // porovnavaci operatory
   TK_EQUAL, // =
@@ -88,43 +84,18 @@ typedef enum {
 } TTokenType;
 
 typedef union {
+  char* name; // identifikator
   int integerValue; // cela cisla
   double doubleValue; // desetinna cisla
   char charValue; // znak 'x'
+  char* stringValue; // string "xxx"
 } TTokenData;
 
 typedef struct {
   TTokenType type; // typ tokenu
-  char* string; // textove data tokenu
-  TTokenData data; //ciselne data tokenu
+  TTokenData data; // data tokenu
   unsigned long int line; // cislo radku, kde se token nachazi
 } TToken;
-
-
-/**
- * @brief Vypis typu tokenu na vystup. (Primarne urceno pro kontrolu.)
- * @param type Typ tokenu, pro vypsani na vystup. 
- */
-void PrintTokenType(TTokenType type);
-
-/**
- * @brief Vypis tokenu na vystup. Konkretne typ tokenu a obsah stringu. (Primarne urceno pro kontrolu.)
- * @param type Token pro vypsani na vystup. 
- */
-void PrintToken(TToken* tkn); 
-
-/**
- * @brief Inicializuje token.
- * @return Ukazatel na inicializovany token, nebo NULL pri chybe.
- */
-TToken* TokenInit();
-
-/**
- * @brief Uvolni ukazatel z pameti i se textovym retezcem v nem.
- * @param s Ukazatel na token.
- */
-void TokenDestroy(TToken* tkn);
-
 
 #endif
 
