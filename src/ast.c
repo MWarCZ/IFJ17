bb59ac114de7e59182568b540cafe03528a772f4
MWarCZ <xvalka05@stud.fit.vutbr.cz>
2017-11-22 13:40:45 +0100

    Vytvoreny soubory ast.* Struktury pro abstraktni syntakticky strom. Funkce Inicializate uzlu stromu. Dve funkce na zniceni stromu/ uvolneni.

---
+++ src/ast.c
@@ -0,0 +1,64 @@
+//////////////////////////////////////////
+// Projekt: IFJ17                       //
+// Soubor: src/ast.c                    //
+// Tým:                                 //
+//   xvalka05 Miroslav Válka            //
+//   xtrnen03 Jan Trněný                //
+//   xproko37 Lukáš Prokop              //
+//   xbarto93 Pavel Bartoň              //
+//////////////////////////////////////////
+
+#ifndef AST_C
+#define AST_C
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast.h"
+#include "token.h"
+
+TATSNode* InitASTNode(TATSNodeType type) {
+  TATSNode *node = NULL;
+  if( (node = malloc(sizeof(TATSNode)) ) == NULL ) {
+    return NULL;
+  }
+  node->type = type;
+  node->token1 = NULL;
+  node->token2 = NULL;
+  node->listPostFix = NULL;
+  node->node1 = NULL;
+  node->node2 = NULL;
+  node->node3 = NULL;
+  node->node4 = NULL;
+
+  return node;
+}
+
+void DestroyASTNode(TATSNode *node) {
+  if( node == NULL ) {
+    return;
+  }
+  DestroyASTNode( node->node1 );
+  DestroyASTNode( node->node2 );
+  DestroyASTNode( node->node3 );
+  DestroyASTNode( node->node4 );
+  free(node);
+}
+
+void DestroyASTNodeSafely(TATSNode **node) {
+  if( node == NULL ) {
+    return;
+  }
+  if( (*node) == NULL ) {
+    return;
+  }
+  DestroyASTNodeSafely( &((*node)->node1) );
+  DestroyASTNodeSafely( &((*node)->node2) );
+  DestroyASTNodeSafely( &((*node)->node3) );
+  DestroyASTNodeSafely( &((*node)->node4) );
+  free( (*node) );
+  (*node) = NULL;
+}
+
+#endif
+
+// Soubor: src/ast.c
---
+++ src/ast.h
@@ -0,0 +1,63 @@
+//////////////////////////////////////////
+// Projekt: IFJ17                       //
+// Soubor: src/ast.h                    //
+// Tým:                                 //
+//   xvalka05 Miroslav Válka            //
+//   xtrnen03 Jan Trněný                //
+//   xproko37 Lukáš Prokop              //
+//   xbarto93 Pavel Bartoň              //
+//////////////////////////////////////////
+
+#ifndef AST_H
+#define AST_H
+
+#include "token.h"
+#include "list.h"
+
+typedef enum {
+  AST_NA = 0,
+  AST_Program,
+  AST_ListDecDef,
+  AST_FunctionHead,
+  AST_ListParam,
+  AST_Param,
+  AST_NextParam,
+  AST_DataType,
+  AST_FunctionEnd,
+  AST_ScopeDef,
+  AST_ScopeHead,
+  AST_ScopeEnd,
+  AST_ScopeAfter,
+  AST_FunctionBody,
+  AST_ListVarDef,
+  AST_VarDefAssigment,
+  AST_ListCommand,
+  AST_Command,
+  AST_ListExpression,
+  AST_Condition,
+  AST_Assignment,
+  AST_ListInParam,
+  AST_InParam,
+  AST_NextInParam,
+  AST_Term,
+  AST_Expression
+} TATSNodeType;
+
+typedef struct TATSNode {
+  TATSNodeType type;
+  TToken *token1;
+  TToken *token2;
+  TList *listPostFix;
+  struct TATSNode *node1;
+  struct TATSNode *node2;
+  struct TATSNode *node3;
+  struct TATSNode *node4;
+} TATSNode;
+
+TATSNode* InitASTNode(TATSNodeType type);
+void DestroyASTNode(TATSNode *node);
+void DestroyASTNodeSafely(TATSNode **node);
+
+#endif
+
+// Soubor: src/ast.h
